261845de990b0cd1d7880e1f565d14e96ee1b803
mrhenry233 <cee.0011@gmail.com>
2025-01-11 22:06:49 +0700

    added th locale

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,11 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 
+import dayjs from "dayjs";
+import "dayjs/locale/th";
+
+dayjs.locale("th");
+
 export default function AppProviders({
   children,
 }: {
