bad91ecbec1f49791391d3a92f4083cd1ec48a86
mrhenry233 <cee.0011@gmail.com>
2025-01-20 12:31:04 +0700

    zod, register form

--- app/common/types.ts
+++ app/common/types.ts
@@ -1,10 +1,10 @@
 import { Role, Sex } from "./enums";
 
 export type ExtendedPropsAppointment = {
-  pet: { id: string; name: string; specie: string; sex: Sex };
-  owner: { id: string; name: string; phone: string };
-  vet: { id: string; name: string; sex: Sex };
-  created_by: { id: string; name: string; position: string };
+  pet: { id: string; name: string; specie: string; sex: Sex; };
+  owner: { id: string; name: string; phone: string; };
+  vet: { id: string; name: string; sex: Sex; };
+  created_by: { id: string; name: string; position: string; };
   appointment: {
     purpose: string;
     notes: string;
@@ -37,8 +37,8 @@ export type Event = {
 };
 
 export type EventTimeForm = {
-  start: { hour: number; minute: number };
-  end: { hour: number; minute: number };
+  start: { hour: number; minute: number; };
+  end: { hour: number; minute: number; };
 };
 
 export type User = {
